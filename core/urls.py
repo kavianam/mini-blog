69906601d8d0e9ba95bbd6aa050d76c95bf325f3
Kavian <kavianam@gmail.com>
2021-08-24 01:50:11 +0430

    redirect all url to blog

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-"""core URL Configuration
+"""mini_blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('', RedirectView.as_view(url='blog/', permanent=True))
 ]
