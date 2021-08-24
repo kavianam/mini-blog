32626db4b1cd80f950fc6029df745bdc644b14ef
Kavian <kavianam@gmail.com>
2021-08-24 20:59:24 +0430

    redirect the root url to /blog/

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('', RedirectView.as_view(url='blog/', permanent=True))
+    path('', RedirectView.as_view(url='blog/', permanent=True)),
 ]
