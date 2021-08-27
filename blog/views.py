adaaed5717a15efa7079fdcca14e05e6ff6d5b6b
Kavian <kavianam@gmail.com>
2021-08-28 02:31:58 +0430

    create inex view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+from .models import Blog, Author
+
+
+def index(request):
+    blogs = Blog.objects.all()
+    authors = Author.objects.all()
+    context = {
+        'blogs': blogs,
+        'authors': authors
+    }
+    return render(request, 'blog/index.html', context)
+    # return render(request, 'blog/test6.html')
