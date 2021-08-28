f4250743cedbfee67a1e89c6d09cb517cacb081a
Kavian <kavianam@gmail.com>
2021-08-28 23:43:24 +0430

    add django authentication urls

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,6 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', RedirectView.as_view(url='blog/', permanent=True)),
 ]
