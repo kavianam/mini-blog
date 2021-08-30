1930dbad1af2d66fcbe9b0a804207a31e8fb80b6
Kavian <kavianam@gmail.com>
2021-08-30 23:16:28 +0430

    add related_names to Blog's author field

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Author(models.Model):
 
 class Blog(models.Model):
     title = models.CharField(max_length=50)
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(Author, related_name='blogs', on_delete=models.SET_NULL, null=True)
     post_date = models.DateField(auto_now_add=True)
     description = models.TextField()
     # comment = models.OneToOneField(Comment, on_delete=models.SET_NULL, null=True, blank=True)
